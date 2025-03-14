66978b9d324c9c122f8017b4d2950c1ce83f2218
nikachu2012 <webmaster@nikachu.net>
2025-03-15 08:13:42 +0900

    [update] セクションタイトルに下線を追加

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function Section({ children, title, id }: { children?: ReactNode, title: ReactNode, id?: string }) {
     return <section id={id}>
-        <h2 className="font-bold text-2xl mb-3">{title}</h2>
-        <div className="flex flex-col flex-wrap gap-2">
+        <h2 className="font-bold text-2xl mb-3 border-b border-neutral-300 pb-1">{title}</h2>
+        <div className="flex flex-col flex-wrap gap-3">
             {children}
         </div>
     </section>
