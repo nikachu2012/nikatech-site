5219d5169aadc9660767b47e67c8e1a95a9272fe
nikachu2012 <webmaster@nikachu.net>
2025-05-18 21:20:20 +0900

    [update] 参加予定イベントを関数で一括管理するように変更

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,6 +5,7 @@ import hero from "./hero.png"
 import HeaderFooter from "@/components/HeaderFooter";
 import GetDateISO8601String from "@/utils/GetDateISO8601String";
 import { NewReleaseList } from "./item/page";
+import { EventSchedule } from "./event/page";
 
 export default function Home() {
   return (
@@ -49,38 +50,7 @@ export default function Home() {
         </Section>
 
         <Section title={"参加予定イベント"} id="event">
-          <div className="border border-neutral-400 p-2">
-            <div className="font-bold text-xl">技術書典18 オフライン会場</div>
-            <div className="">2025-06-01 (日) / 池袋・サンシャインシティ 文化会館ビル2F 展示ホールD / <span className="text-red-500">う17</span></div>
-
-            <div className="flex flex-wrap my-1 gap-2">
-              <Link href={"https://techbookfest.org/event/tbf18"} target="_blank">イベントの公式ページ</Link>
-              <Link href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link>
-            </div>
-
-            <table className="border-collapse mt-2 table-auto">
-              <thead>
-                <tr>
-                  <th className="border border-neutral-400 p-1 px-2">種別</th>
-                  <th className="border border-neutral-400 p-1 px-2">商品名</th>
-                  <th className="border border-neutral-400 p-1 px-2">値段</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr>
-                  <td className="border border-neutral-400 p-1 px-2">新刊</td>
-                  <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/avrasm"}>Arduinoで学ぶアセンブリ</Link></td>
-                  <td className="border border-neutral-400 p-1 px-2">JPY 500</td>
-                </tr>
-                <tr>
-                  <td className="border border-neutral-400 p-1 px-2">新刊</td>
-                  <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/smartremote_guide"}>スマートリモコン自作ガイド</Link></td>
-                  <td className="border border-neutral-400 p-1 px-2">JPY 500</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <EventSchedule />
         </Section>
 
 
