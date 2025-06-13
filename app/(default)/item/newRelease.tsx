cddde879472834dc8e37fafad187f8896a526ad8
nikachu2012 <webmaster@nikachu.net>
2025-06-13 23:03:21 +0900

    [update] 新刊のステータス的なものを削除

--- app/(default)/item/newRelease.tsx
+++ app/(default)/item/newRelease.tsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 export function NewReleaseList() {
     return <>
         <ul className="list-disc list-outside ml-6">
-            <li><Link href={"item/smartremote_guide"}>スマートリモコン自作ガイド（届いたよ）</Link></li>
+            <li><Link href={"item/smartremote_guide"}>スマートリモコン自作ガイド</Link></li>
             <li>
-                <Link href={"item/avrasm"}>Arduinoで始めるアセンブリ（届いたよ）</Link><br />
+                <Link href={"item/avrasm"}>Arduinoで始めるアセンブリ</Link><br />
                 サークル代表へ　アセンブラじゃなくてアセンブ<b>リ</b>ですご注意を
             </li>
             <li><Link href={"item/unicode_animal_paper"}>Unicode 動物絵文字 コードポイント表</Link><br /></li>
