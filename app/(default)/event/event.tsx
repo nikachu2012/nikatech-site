9febb8d7c5c34ee27565c44e4c9ad0d3f3a1125e
nikachu2012 <webmaster@nikachu.net>
2025-06-13 23:01:10 +0900

    [update] 技術書典18オフライン会場終了に伴うイベントページの更新

--- app/(default)/event/event.tsx
+++ app/(default)/event/event.tsx
@@ -1,46 +1,31 @@
-import Link from "next/link";
 
 export function EventSchedule() {
     return <>
-        <div className="border border-neutral-400 p-2">
+        沼津高専 第60回高専祭（委託）、技術書典19、コミックマーケット107（多分委託）に出るかも
+        {/* <div className="border border-neutral-400 p-2">
             <div className="font-bold text-xl">技術書典18 オフライン会場</div>
-            <div className="">2025-06-01 (日) / 池袋・サンシャインシティ 文化会館ビル2F 展示ホールD / う17</div>
+            <div className="">2025-06-01 (日) / 審査中です</div>
+            <div>
 
-            <div className="flex flex-wrap my-1 gap-2">
-                <Link href={"https://techbookfest.org/event/tbf18"} target="_blank">イベントの公式ページ</Link>
-                <Link href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link>
+                <table className="border-collapse mt-1 table-auto">
+                    <thead>
+                        <tr>
+                            <th className="border border-neutral-400 p-1 px-2">種別</th>
+                            <th className="border border-neutral-400 p-1 px-2">商品名</th>
+                            <th className="border border-neutral-400 p-1 px-2">値段</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                            <td className="border border-neutral-400 p-1 px-2">ンゴシリーズ vol.1</td>
+                            <td className="border border-neutral-400 p-1 px-2">500 JPY</td>
+                        </tr>
+                    </tbody>
+                </table>
+
             </div>
-
-            <table className="border-collapse mt-2 table-auto">
-                <thead>
-                    <tr>
-                        <th className="border border-neutral-400 p-1 px-2">種別</th>
-                        <th className="border border-neutral-400 p-1 px-2">商品名</th>
-                        <th className="border border-neutral-400 p-1 px-2">値段</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <tr>
-                        <td className="border border-neutral-400 p-1 px-2">新刊</td>
-                        <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/avrasm"}>Arduinoで始めるアセンブリ</Link></td>
-                        <td className="border border-neutral-400 p-1 px-2">JPY 500</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-neutral-400 p-1 px-2">新刊</td>
-                        <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/smartremote_guide"}>スマートリモコン自作ガイド</Link></td>
-                        <td className="border border-neutral-400 p-1 px-2">JPY 500</td>
-                    </tr>
-                    <tr>
-                        <td className="border border-neutral-400 p-1 px-2">新刊</td>
-                        <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/unicode_animal_paper"}>Unicode 動物絵文字 コードポイント表</Link></td>
-                        <td className="border border-neutral-400 p-1 px-2">JPY 100</td>
-                    </tr>
-                </tbody>
-            </table>
-            {/* <div className="mt-2">
-                <RealtimeEvent />
-            </div> */}
-        </div>
+        </div> */}
     </>
 }
--- app/(default)/event/page.tsx
+++ app/(default)/event/page.tsx
@@ -2,6 +2,7 @@ import Section from "@/components/Section";
 import HeaderFooter from "@/components/HeaderFooter";
 import { Metadata } from "next";
 import { EventSchedule } from "./event";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: "イベント",
@@ -13,34 +14,49 @@ export default function Home() {
             <main className="flex flex-col flex-wrap gap-6 py-4">
                 <Section title={"参加予定イベント"} id="event">
                     <EventSchedule />
-
-                    {/* <div className="border border-neutral-400 p-2">
-                        <div className="font-bold text-xl">技術書典18 オフライン会場</div>
-                        <div className="">2025-06-01 (日) / 審査中です</div>
-                        <div>
-
-                            <table className="border-collapse mt-1 table-auto">
-                                <thead>
-                                    <tr>
-                                        <th className="border border-neutral-400 p-1 px-2">種別</th>
-                                        <th className="border border-neutral-400 p-1 px-2">商品名</th>
-                                        <th className="border border-neutral-400 p-1 px-2">値段</th>
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <tr>
-                                        <td className="border border-neutral-400 p-1 px-2">新刊</td>
-                                        <td className="border border-neutral-400 p-1 px-2">ンゴシリーズ vol.1</td>
-                                        <td className="border border-neutral-400 p-1 px-2">500 JPY</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                        </div>
-                    </div> */}
                 </Section>
+                <Section title={"イベント参加歴"} id="history">
+                    <div className="border border-neutral-400 p-2">
+                        <div className="font-bold text-xl">技術書典18 オフライン会場</div>
+                        <div className="">2025-06-01 (日) / 池袋・サンシャインシティ 文化会館ビル2F 展示ホールD / う17</div>
 
+                        <div className="flex flex-wrap my-1 gap-2">
+                            <Link href={"https://techbookfest.org/event/tbf18"} target="_blank">イベントの公式ページ</Link>
+                            <Link href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link>
+                        </div>
+
+                        <table className="border-collapse mt-2 table-auto">
+                            <thead>
+                                <tr>
+                                    <th className="border border-neutral-400 p-1 px-2">種別</th>
+                                    <th className="border border-neutral-400 p-1 px-2">商品名</th>
+                                    <th className="border border-neutral-400 p-1 px-2">値段</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/avrasm"}>Arduinoで始めるアセンブリ</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2">JPY 500</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/smartremote_guide"}>スマートリモコン自作ガイド</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2">JPY 500</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link href={"/item/unicode_animal_paper"}>Unicode 動物絵文字 コードポイント表</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2">JPY 100</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        {/* <div className="mt-2">
+                <RealtimeEvent />
+            </div> */}
+                    </div>
+                </Section>
             </main>
         </HeaderFooter>
     );
