d0166d172f2d3639a2f9d068e987e519d56381f3
nikachu2012 <webmaster@nikachu.net>
2025-06-27 22:20:52 +0900

    [update] 気に食わなかったのでメイン要素の周りの影を削除

--- components/HeaderFooter.tsx
+++ components/HeaderFooter.tsx
@@ -56,7 +56,7 @@ export default function HeaderFooter({ children }: { children: ReactNode }) {
         </div>
         <div className="w-full lg:w-[1000px] m-auto">
 
-            <div className="w-full bg-white m-auto px-4 pb-4 shadow">
+            <div className="w-full bg-white m-auto px-4 pb-4">
                 {children}
             </div>
 
