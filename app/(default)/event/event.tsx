38e6175235b2122979aec259fe317a8ebf42617b
nikachu2012 <webmaster@nikachu.net>
2025-09-08 20:09:47 +0900

    [fix] 先に開催されるイベントを上に持ってくるように変更

--- app/(default)/event/event.tsx
+++ app/(default)/event/event.tsx
@@ -4,14 +4,8 @@ export function EventSchedule() {
     return <>
         コミックマーケット107（委託じゃないですよ！！）に出るかも
         <div className="border border-neutral-400 p-2">
-            <div className="font-bold text-xl">技術書典19 オフライン会場</div>
-            <div className="">2025-11-16 (日) / 審査中です</div>
-
-            <div className="flex flex-wrap my-1 gap-2">
-                <Link className="link" href={"https://techbookfest.org/event/tbf19"} target="_blank">イベントの公式ページ</Link>
-                <Link className="link" href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link>
-            </div>
-
+            <div className="font-bold text-xl">【委託】沼津高専 第60回高専祭</div>
+            <div className="">2025-11-08 (土), 2025-11-09 (日) / 沼津高専 / 講義等1階 専攻科101, 102 / <Link className="link" href={"https://csc.nikachu.net"} target="_blank">情報工学同好会 (非公認)</Link>への委託</div>
             <div>
                 頒布品未定
                 {/* <table className="border-collapse mt-1 table-auto">
@@ -36,8 +30,14 @@ export function EventSchedule() {
         </div>
 
         <div className="border border-neutral-400 p-2">
-            <div className="font-bold text-xl">【委託】沼津高専 第60回高専祭</div>
-            <div className="">2025-11-08 (土), 2025-11-09 (日) / 沼津高専 / 講義等1階 専攻科101, 102 / <Link className="link" href={"https://csc.nikachu.net"} target="_blank">情報工学同好会 (非公認)</Link>への委託</div>
+            <div className="font-bold text-xl">技術書典19 オフライン会場</div>
+            <div className="">2025-11-16 (日) / 審査中です</div>
+
+            <div className="flex flex-wrap my-1 gap-2">
+                <Link className="link" href={"https://techbookfest.org/event/tbf19"} target="_blank">イベントの公式ページ</Link>
+                <Link className="link" href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link>
+            </div>
+
             <div>
                 頒布品未定
                 {/* <table className="border-collapse mt-1 table-auto">
