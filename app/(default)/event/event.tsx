7d5e90cf1a00f1ca0b798be0bdf510031784d0df
nikachu2012 <webmaster@nikachu.net>
2025-11-02 18:24:17 +0900

    [add] 沼津高専第60回高専祭の頒布品にhowto_isdnの項目を追加

--- app/(default)/event/event.tsx
+++ app/(default)/event/event.tsx
@@ -5,7 +5,7 @@ export function EventSchedule() {
         <div className="border border-neutral-400 p-2">
             <div className="font-bold text-xl">【委託】沼津高専 第60回高専祭</div>
             <div className="">2025-11-08 (土), 2025-11-09 (日) / 沼津高専 講義等1階 専攻科101, 102 / <Link className="link" href={"https://csc.nikachu.net"} target="_blank">情報工学同好会 (非公認)</Link>への委託</div>
-            
+
             <div className="flex flex-wrap my-1 gap-2">
                 <Link className="link" href={"https://www.numazu-ct.ac.jp/news/12921/"} target="_blank">イベントの公式ページ</Link>
                 {/* <Link className="link" href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link> */}
@@ -25,6 +25,11 @@ export function EventSchedule() {
                         <td className="border border-neutral-400 p-1 px-2">新刊</td>
                         <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/llvm_homebrew_lang"}>LLVMで作るプログラミング言語</Link></td>
                         <td className="border border-neutral-400 p-1 px-2 text-right">JPY 1,500</td>
+                    </tr>
+                    <tr>
+                        <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                        <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/howto_isdn"}>ISDNを正しく表記してガチの本みたいな同人誌を作ろう</Link></td>
+                        <td className="border border-neutral-400 p-1 px-2 text-right">JPY 100</td>
                     </tr>
                     <tr>
                         <td className="border border-neutral-400 p-1 px-2">既刊</td>
