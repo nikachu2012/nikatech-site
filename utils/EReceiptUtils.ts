cf1d369f098227e8b9b5c5269878dfa0fa673a3e
nikachu2012 <webmaster@nikachu.net>
2026-04-11 21:46:11 +0900

    [add] Squareギフトカードの決済方法を追加

--- utils/EReceiptUtils.ts
+++ utils/EReceiptUtils.ts
@@ -29,43 +29,60 @@ export type Receipt = {
 
 export function paymentToReadableString(t: string): string {
     switch (t) {
+        // その他
         case "CASH":
-            return "現金"
+            return "現金";
         case "CREDIT":
-            return "クレジット"
+            return "国際ブランド";
         case "BARCODE":
-            return "QRコード"
-        case "CREDIT_AIRPAY":
-            return "クレジット(Airペイ)"
-        case "CREDIT_AIRPAY_TOUCH":
-            return "クレ(Airペイタッチ)"
-        case "CREDIT_UNIONPAY_AIRPAY":
-            return "銀聯(Airペイ)"
-        case "BARCODE_AIRPAY":
-            return "QRコード(Airペイ)"
-        case "QUICPAY_AIRPAY":
-            return "QUICPay+(Airペイ)"
-        case "BARCODE_AIRPAYQR":
-            return "QRコード(AirQR)"
-        case "CREDIT_SQ":
-            return "クレジット(Square)"
-        case "CREDIT_SQ_IPHONE":
-            return "クレ(SQ TaptoPay iPhone)"
-        case "CREDIT_SQ_ANDROID":
-            return "クレ(SQ TaptoPay Android)"
-        case "ID_SQ":
-            return "iD(Square)"
-        case "QUICPAY_SQ":
-            return "QUICPay+(Square)"
-        case "TRANSPORTATION_IC_SQ":
-            return "交通系IC(Square)"
+            return "1,2次元コード";
         case "TECHBOOKFEST":
-            return "技術書典公式後払い"
+            return "技術書典公式後払い";
+
+        // Airペイ
+        case "CREDIT_AIRPAY":
+            return "クレジット(Airペイ)";
+        case "CREDIT_AIRPAY_TOUCH":
+            return "クレ(Airペイタッチ)";
+        case "CREDIT_UNIONPAY_AIRPAY":
+            return "銀聯(Airペイ)";
+        case "BARCODE_AIRPAY":
+            return "QRコード(Airペイ)";
+        case "QUICPAY_AIRPAY":
+            return "QUICPay+(Airペイ)";
+
+        // Airペイ QR
+        case "BARCODE_AIRPAYQR":
+            return "QRコード(AirQR)";
+
+        // Squareタ
+        case "CREDIT_SQ":
+            return "クレジット(Square)";
+        case "ID_SQ":
+            return "iD(Square)";
+        case "QUICPAY_SQ":
+            return "QUICPay+(Square)";
+        case "TRANSPORTATION_IC_SQ":
+            return "交通系IC(Square)";
+        case "BARCODE_SQ":
+            return "QRコード(Square)";
+
+        // Square他
+        case "CREDIT_SQ_MN":
+            return "クレ(Square MN)";
+        case "CREDIT_SQ_MS":
+            return "クレ(Sq MSリーダ)";
+        case "CREDIT_SQ_IPHONE":
+            return "クレ(Sq Tap to Pay iPhone)";
+        case "CREDIT_SQ_ANDROID":
+            return "クレ(Sq Tap to Pay Android)";
+        case "GIFTCARD_SQ":
+            return "ギフトカード(Square)";
+
         default:
-            return t
+            return t; 
     }
 }
-
 export function paymentToSlipString(t: string): string {
     switch (t) {
         case "CREDIT_SQ":
