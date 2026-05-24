f261065d32848657173db125e3296a2169981fbc
nikachu2012 <webmaster@nikachu.net>
2026-05-25 00:44:16 +0900

    [update] 技術書典20のイベント情報を移動

--- app/(default)/event/page.tsx
+++ app/(default)/event/page.tsx
@@ -17,6 +17,66 @@ export default function Home() {
                 </Section>
                 <Section title={"イベント参加歴"} id="history">
                     <div className="border border-neutral-400 p-2">
+                        <div className="font-bold text-xl">技術書典20 オンライン＋オフライン</div>
+                        <div className="">2026-04-12 (日) / 池袋・サンシャインシティ 文化会館ビル2F 展示ホールD / <b>あ01</b></div>
+                        <div className="text-red-600">サークル代表の体調不良のため欠席するかもしれません。許してね</div>
+
+                        <div className="flex flex-wrap my-1 gap-2">
+                            <Link className="link" href={"https://techbookfest.org/event/tbf20"} target="_blank">イベントの公式ページ</Link>
+                            <Link className="link" href={"https://techbookfest.org/organization/KdLZ1mQYzBTzyyz68x2UX"} target="_blank">サークルページ</Link>
+                        </div>
+
+
+                        <div>
+                            {/* 頒布品未定（新刊は２冊を予定しています。またスマートリモコン自作ガイドについては電子版のみの販売となる予定です。） */}
+                            <table className="border-collapse mt-1 table-auto">
+                                <thead>
+                                    <tr>
+                                        <th className="border border-neutral-400 p-1 px-2">種別</th>
+                                        <th className="border border-neutral-400 p-1 px-2">商品名</th>
+                                        <th className="border border-neutral-400 p-1 px-2">値段</th>
+                                    </tr>
+                                </thead>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/gba_lowlevel"}>GBAで学ぶ低レイヤー</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 1,500</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">新刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/doujin_pos_cashless"}>スマート頒布 〜同人誌即売会に自作POSとキャッシュレス決済を入れてみた〜</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 500</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">新商品</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/gba_bootcable"}>GBAブートケーブル</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 1,000</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">既刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/llvm_homebrew_lang"}>LLVMで作るプログラミング言語</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 1,500</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">既刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/howto_isdn"}>ISDNを正しく表記してガチの本みたいな同人誌を作ろう</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 100</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">既刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/avrasm"}>Arduinoで始めるアセンブリ</Link></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 500</td>
+                                </tr>
+                                <tr>
+                                    <td className="border border-neutral-400 p-1 px-2">既刊</td>
+                                    <td className="border border-neutral-400 p-1 px-2"><Link className="link" href={"/item/smartremote_guide"}>スマートリモコン自作ガイド</Link><b>（電子版のみ）</b></td>
+                                    <td className="border border-neutral-400 p-1 px-2 text-right">JPY 500</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+
+                    <div className="border border-neutral-400 p-2">
                         <div className="font-bold text-xl">コミックマーケット107</div>
                         <div className="">2025-12-31 (水) / 東京ビッグサイト / 南2 i-19b</div>
                         <div className="text-red-600">サークル代表は、体調不良のため出展場所にはいません。大変申し訳ありません。</div>
